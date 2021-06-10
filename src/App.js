025a8dcc09da0f568da3362e3018bcc8a4dd23ba
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-10 16:47:31 -0400

    add use context to Home.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useContext } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
+	const ctx = useContext(AuthContext);
 	return (
 		<>
 			<MainHeader onLogout={logoutHandler} />
 			<main>
-				{!isLoggedIn && <Login onLogin={loginHandler} />}
-				{isLoggedIn && <Home onLogout={logoutHandler} />}
+				{!ctx && <Login />}
+				{ctx && <Home />}
 			</main>
 		</>
 	);
